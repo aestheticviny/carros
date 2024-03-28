9128e8e39202f5aacb58769c73da029ce867887d
Vincicius Paixão <vinisantospb404@gmail.com>
2024-03-28 05:38:44 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ export default function app(){
     return(
 
       <main className="nin-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center itens-center">
-        <section className="lg:flex max-w-[920px]">
+        <div className="lg:flex max-w-[920px]">
           <Sedans />
           <Suvs />
           <Luxuosos />
 
-        </section>
+        </div>
       </main>
     )
 
